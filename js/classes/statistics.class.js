98817262cf3ac11eb5cc466d382ccd7a27efa35b
KLCHOST <52049111+KLCHOST@users.noreply.github.com>
2019-06-20 16:53:23 -0500

    Update statistics.class.js

--- js/classes/statistics.class.js
+++ js/classes/statistics.class.js
@@ -1,36 +1,36 @@
 class Statistics {
     static async getTotalCars() {
-        const req = await fetch('http://72.27.52.224/car/php/getTotalCars.php')
+        const req = await fetch('https://klcvehiclemovements.klclogistics-jm.com/klc_vehicle_movements/getTotalCars.php')
         const res = await req.json()
         return res 
     }
     static async getNewCarsToday() {
-        const req = await fetch('http://72.27.52.224/car/php/getNewCarsToday.php')
+        const req = await fetch('https://klcvehiclemovements.klclogistics-jm.com/klc_vehicle_movements/getNewCarsToday.php')
         const res = await req.json()
         return res
     }
     static async getReleasedToday() {
-        const req = await fetch('http://72.27.52.224/car/php/getReleasedToday.php')
+        const req = await fetch('https://klcvehiclemovements.klclogistics-jm.com/klc_vehicle_movements/getReleasedToday.php')
         const res = await req.json()
         return res
     }
     static async getNewCarsWeek() {
-        const req = await fetch('http://72.27.52.224/car/php/getNewCarsWeek.php')
+        const req = await fetch('https://klcvehiclemovements.klclogistics-jm.com/klc_vehicle_movements/getNewCarsWeek.php')
         const res = await req.json()
         return res
     }
     static async getReleaseCarsWeek() {
-        const req = await fetch('http://72.27.52.224/car/php/getReleaseCarsWeek.php')
+        const req = await fetch('https://klcvehiclemovements.klclogistics-jm.com/klc_vehicle_movements/getReleaseCarsWeek.php')
         const res = await req.json()
         return res
     }
     static async getNewCarsMonth() {
-        const req = await fetch('http://72.27.52.224/car/php/getNewCarsMonth.php')
+        const req = await fetch('https://klcvehiclemovements.klclogistics-jm.com/klc_vehicle_movements/getNewCarsMonth.php')
         const res = await req.json()
         return res
     }
     static async getReleaseCarsMonth() {
-        const req = await fetch('http://72.27.52.224/car/php/getReleaseCarsMonth.php')
+        const req = await fetch('https://klcvehiclemovements.klclogistics-jm.com/klc_vehicle_movements/getReleaseCarsMonth.php')
         const res = await req.json()
         return res
     }
